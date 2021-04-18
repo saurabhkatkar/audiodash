b474c13dee19db8a9ab9588be3b68a41847eb45e
Saurabh Katkar <saurabh.katkar102@gmail.com>
2021-04-18 20:37:43 +0530

    new changes

---
+++ lib/repository/constants.dart
@@ -0,0 +1,50 @@
+List<String> urls = [
+  "https://firebasestorage.googleapis.com/v0/b/musicplayer-be37b.appspot.com/o/songs%2FBack%20in%20Black.mp3?alt=media&token=6bb8cc8d-ba5c-452d-a664-9f8f4753a9df",
+  "https://firebasestorage.googleapis.com/v0/b/musicplayer-be37b.appspot.com/o/songs%2FCounting%20Stars%20(Alex%20Goot).mp3?alt=media&token=86078c04-e87c-4ab0-9cc0-ba97eecaafaa",
+  "https://firebasestorage.googleapis.com/v0/b/musicplayer-be37b.appspot.com/o/songs%2FFight%20Song.mp3?alt=media&token=4d4ece87-a100-48b5-b35c-445014f6c7c3",
+  "https://firebasestorage.googleapis.com/v0/b/musicplayer-be37b.appspot.com/o/songs%2FLearn%20To%20Lose.mp3?alt=media&token=fed4a184-c54b-4d91-bdd6-f668d0702575",
+  "https://firebasestorage.googleapis.com/v0/b/musicplayer-be37b.appspot.com/o/songs%2FLone%20Digger.mp3?alt=media&token=ae749e33-8990-48d2-8190-ef3f4587948d",
+  "https://firebasestorage.googleapis.com/v0/b/musicplayer-be37b.appspot.com/o/songs%2FOld%20Town%20Road.mp3?alt=media&token=30dbfa20-dbbe-41a3-a64f-eeaea4500b30",
+  "https://firebasestorage.googleapis.com/v0/b/musicplayer-be37b.appspot.com/o/songs%2FSparkle.mp3?alt=media&token=dac96a15-dc07-4d55-bb99-befefd673cfc",
+  "https://firebasestorage.googleapis.com/v0/b/musicplayer-be37b.appspot.com/o/songs%2FSummer%20Nights.mp3?alt=media&token=c1d214d1-052a-49b0-97ad-1dd99bd6db69",
+  "https://firebasestorage.googleapis.com/v0/b/musicplayer-be37b.appspot.com/o/songs%2FThe%20Wolf.mp3?alt=media&token=a02c23ae-3a0f-43fd-8a79-1d725bb6dcd6",
+  "https://firebasestorage.googleapis.com/v0/b/musicplayer-be37b.appspot.com/o/songs%2FWake%20Me%20Up.mp3?alt=media&token=a41329e1-a2ed-4cb9-bf17-4a348c8f6d2a"
+];
+
+List<String> covers = [
+  "https://firebasestorage.googleapis.com/v0/b/musicplayer-be37b.appspot.com/o/covers%2Falbum4.jpg?alt=media&token=feb4c444-aa86-4d2f-bab0-744a9c0d42b7",
+  "https://firebasestorage.googleapis.com/v0/b/musicplayer-be37b.appspot.com/o/covers%2Fcounting%20stars.jpg?alt=media&token=54240b1b-3b8e-4ae1-8770-d4d5940aa15f",
+  "https://firebasestorage.googleapis.com/v0/b/musicplayer-be37b.appspot.com/o/covers%2Ffight%20song.jpg?alt=media&token=d4ac1f0b-faef-40d1-9de1-c0278042a5a7",
+  "https://firebasestorage.googleapis.com/v0/b/musicplayer-be37b.appspot.com/o/covers%2Flearn%20to%20lose.jpg?alt=media&token=83e0c40e-8ab8-427d-8abd-782f26ba72f4",
+  "https://firebasestorage.googleapis.com/v0/b/musicplayer-be37b.appspot.com/o/covers%2Flone%20digger.jpg?alt=media&token=378d7562-3bda-4c00-a7e7-32910ec0a6a2",
+  "https://firebasestorage.googleapis.com/v0/b/musicplayer-be37b.appspot.com/o/covers%2Fold%20town%20road.jpg?alt=media&token=7da0ed26-f8ca-4fa3-b525-80ada8a3c711",
+  "https://firebasestorage.googleapis.com/v0/b/musicplayer-be37b.appspot.com/o/covers%2Fsparkle.jpg?alt=media&token=ff511f09-3099-4a91-900d-812f47b96bc4",
+  "https://firebasestorage.googleapis.com/v0/b/musicplayer-be37b.appspot.com/o/covers%2Fsummer%20nights.jpg?alt=media&token=8b380586-1929-4ada-a9b5-bc87e3ad8e65",
+  "https://firebasestorage.googleapis.com/v0/b/musicplayer-be37b.appspot.com/o/covers%2Fthe%20wolf.jpg?alt=media&token=8d6db82b-3c17-4643-b759-54b883ad896d",
+  "https://firebasestorage.googleapis.com/v0/b/musicplayer-be37b.appspot.com/o/covers%2Fwake%20me%20up.jpg?alt=media&token=c4de478a-9810-4e20-b575-1c32dddbafa1"
+];
+
+List<String> titles = [
+  "Back In Back",
+  "Counting Stars",
+  "Fight Song",
+  "Learn to Lose",
+  "Lone Digger",
+  "Old Town Road",
+  "Sparkle",
+  "Summer Nights",
+  "The Wolf",
+  "Wake Me Up"
+];
+List<String> artists = [
+  "AC/DC",
+  "OneRepublic - Alex Goot",
+  "Rachel Platten",
+  "Bakermat",
+  "Caravan Palace",
+  "Lil Nas X",
+  "RADWIMPS",
+  "SIAMÉS",
+  "SIAMÉS",
+  "Avicii"
+];
